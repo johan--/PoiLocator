139ce865da6d23fd5605c45f2fc02712443f707f
oliverheilig <oliver.heilig@yahoo.de>
2015-03-16 14:19:40 +0100

    for gh-pages

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
 // This token is valid until 2015-01-29T19:05:59Z and was auto-generated @ 2014-11-26T22:26:00Z.
-var token = '';
+var token = '333741081737732';
