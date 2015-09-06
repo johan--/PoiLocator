2a6a80ac6e96f4f0fd402a7fecd81865f6187819
unknown <Marvin@wn-esx-mmbuild.ptvag.ptv.de>
2015-09-07 00:46:27 +0200

    Token update

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
-// This token is valid until 2015-11-06T21:35:06Z and was auto-generated @ 2015-09-05T21:33:42Z.
+// This token is valid until 2015-11-06T21:35:06Z and was auto-generated @ 2015-09-06T21:32:41Z.
 var token = '561677741926322';
