e95e83b4730648e756f3e1c80ebdf99db5de3cf5
xserverinternet <oliver.heilig@ptvgroup.com>
2015-09-20 00:46:18 +0200

    Token update

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
-// This token is valid until 2015-12-04T22:09:55Z and was auto-generated @ 2015-09-18T21:48:43Z.
+// This token is valid until 2015-12-04T22:09:55Z and was auto-generated @ 2015-09-19T21:46:23Z.
 var token = '296799511224615';
